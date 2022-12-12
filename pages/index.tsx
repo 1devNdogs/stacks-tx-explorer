436f5c5a7958bf6fce48b8eceb855ca03c7b5dab
1devndogs <vsalgado.a@gmail.com>
2022-12-12 18:40:53 -0300

    ws client connect to mempool and transaction

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 import { WalletConnectButton } from "../components/wallet-connect-button";
 import { UserCard } from "../components/user-card";
 import { getDehydratedStateFromSession } from "../common/session-helpers";
+import { TransactionTable } from "../components/transaction-table";
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   return {
@@ -18,6 +19,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <UserCard />
         <WalletConnectButton />
+        <TransactionTable/>
       </main>
     </div>
   );
