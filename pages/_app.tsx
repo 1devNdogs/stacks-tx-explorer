53eb4166c7c61dd79ca7ab0d7b53c526320f0369
1devndogs <vsalgado.a@gmail.com>
2022-12-12 15:37:34 -0300

    Auth session callback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ClientProvider } from "@micro-stacks/react";
+import { destroySession, saveSession } from "../common/fetchers";
+import { useCallback } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClientProvider appName="Nextjs + Microstacks" appIconUrl="/vercel.png">
+    <ClientProvider
+      appName="Nextjs + Microstacks"
+      appIconUrl="/vercel.png"
+      dehydratedState={pageProps?.dehydratedState}
+      onPersistState={useCallback(async (dehydratedState: string) => {
+        await saveSession(dehydratedState);
+      }, [])}
+      onSignOut={useCallback(async () => {
+        await destroySession();
+      }, [])}
+    >
       <Component {...pageProps} />
     </ClientProvider>
   );
